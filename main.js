1b3814f827523df58c97165c38c3eedf8aaaffac
Rapha <11071770+Resousse@users.noreply.github.com>
2025-11-15 22:01:27 +0100

    Upgrade dependencies versions to solve blocking issues (#3)
    * Upgrade of versions
    
    * Update action name to include version upgrade
    
    * Remove version upgrade mention from action name
    
    * Adding a sleep time to avoid rate limit
    
    * Update action name to include version upgrade
    
    * Finally it's better with child page parsing
    
    * recompiling index.js
    
    * With only notion client
    
    * rate limit of 1000
    
    * testing with 2sec time
    
    * Adding pagination to the upgrade

--- main.js
+++ main.js
@@ -1,23 +1,50 @@
 const { Client } = require('@notionhq/client');
+const core = require('@actions/core');
 const { NotionToMarkdown } = require('notion-to-md');
+const { backOff } = require('exponential-backoff');
 const fs = require('fs').promises;
 
 const notionClient = new Client({ auth: process.env['INPUT_NOTION-TOKEN'] });
 
-const notionToMarkdown = new NotionToMarkdown({ notionClient });
+const notionToMarkdown = new NotionToMarkdown({ 
+  notionClient: notionClient,
+    config:{
+     separateChildPage:true
+  }
+ });
 
 (async () => {
-  const search = await notionClient.search({});
+  let arguments = { };
 
-  search.results.forEach (async result => {
-    const baseName = result.url.split('/').pop();
+  let search = { };
 
-    const mdBlocks = await notionToMarkdown.pageToMarkdown(result.id);
+  do {
+    search = await notionClient.search(arguments);
 
-    const { parent } = notionToMarkdown.toMarkdownString(mdBlocks);
+    for (const result of search.results) {
+      const baseName = result.url.split('/').pop();
 
-    if(typeof parent !== "undefined") {
-      await fs.writeFile(`${baseName}.md`, parent);
+      const backoffOptions = {
+        startingDelay: 60 * 60 * 1000,
+        numOfAttempts: 2,
+        timeMultiple: 1,
+      };
+
+      const mdBlocks = await backOff(() => notionToMarkdown.pageToMarkdown(result.id), backoffOptions);
+
+      try {
+        const { parent } = notionToMarkdown.toMarkdownString(mdBlocks);
+
+        if(typeof parent !== "undefined") {
+          await fs.writeFile(`${baseName}.md`, parent);
+        }
+      } catch (error) {
+        core.warning(`Conversion to markdown failed for: ${result.url}`);
+      }
     }
-  });
+
+    if (search.has_more) {
+      arguments = { start_cursor: search.next_cursor };
+    }
+  } while (search.has_more);
 })();
